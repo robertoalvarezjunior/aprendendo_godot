270fda9ae2e046a14458f7e0873fe3f10c76dc7a
robertoalvarezjunior <robertojr.colorado@gmail.com>
2026-06-11 23:06:02 -0300

    signal

--- scripts/LevelEnd.cs
+++ scripts/LevelEnd.cs
@@ -4,24 +4,37 @@ using Godot;
 public partial class LevelEnd : Area2D
 {
 
-    [Export]
-    String nextLevelScene = "";
+	[Export]
+	String nextLevelScene = "";
 
-    public override void _Ready()
-    {
-        Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
-    }
+	[Signal]
+	public delegate void LevelCompletedEventHandler();
 
-    private void OnBodyEntered(Node body)
-    {
-        if (body.IsInGroup("Player"))
-        {
-            CallDeferred("loadNextLevel");
-        }
-    }
+	public override void _Ready()
+	{
+		LevelCompleted += () => GD.Print("Level Completed!");
+		BodyEntered += OnBodyEntered2;
+	}
 
-    private void loadNextLevel()
-    {
-        GetTree().ChangeSceneToFile($"res://scene/{nextLevelScene}.tscn");
-    }
+	// private void OnBodyEntered(Node body)
+	// {
+	// 	if (body.IsInGroup("Player"))
+	// 	{
+	// 		EmitSignal(SignalName.LevelCompleted);
+	// 		CallDeferred("LoadNextLevel");
+	// 	}
+	// }
+	private void OnBodyEntered2(Node body)
+	{
+		if (body.IsInGroup("Player"))
+		{
+			EmitSignal(SignalName.LevelCompleted);
+			CallDeferred("LoadNextLevel");
+		}
+	}
+
+	private void LoadNextLevel()
+	{
+		GetTree().ChangeSceneToFile($"res://scene/{nextLevelScene}.tscn");
+	}
 }
